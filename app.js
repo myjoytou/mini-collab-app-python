5feacfcf9961ebb087803fe36495db4e9876d90d
Vivek Kumar <vivek.kumar@infibeam.net>
2016-09-14 15:24:02 +0530

    Removing logs

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
     console.log('coming here');
-    // var address = prompt("enter your address");
     var mySocket = new WebSocket("ws://" + location.host + "/ws");
-    // var mySocket = new WebSocket("ws://localhost:8080/ws");
     mySocket.onmessage = function (response) {
-        console.log(response.type);
         $('#document').val(response.data);
     };
     $('#document').keyup(function (event) {
-        console.log('coming here');
         var doc = $(this).val();
         mySocket.send(doc);
         event.preventDefault();
